9399348273ac7468590217c2193c30f75042aa5a
Alen Siljan <alen.siljan@gmail.com>
2017-02-20 22:46:23 +0100

    Add code for fetching all github repos

--- app/src/main/java/com/siljan/alen/mvpdaggersample/mvp/interactors/impl/RepoListInteractorImpl.java
+++ app/src/main/java/com/siljan/alen/mvpdaggersample/mvp/interactors/impl/RepoListInteractorImpl.java
@@ -1,7 +1,12 @@
 package com.siljan.alen.mvpdaggersample.mvp.interactors.impl;
 
+import com.siljan.alen.mvpdaggersample.data.models.GithubRepoModel;
 import com.siljan.alen.mvpdaggersample.mvp.interactors.IRepoListInteractor;
 import com.siljan.alen.mvpdaggersample.mvp.listeners.RepoListListener;
+import com.siljan.alen.mvpdaggersample.networking.ApiManager;
+import com.siljan.alen.mvpdaggersample.networking.callbacks.GithubRepositoriesCallback;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -10,17 +15,30 @@ import javax.inject.Inject;
  * alen.siljan@gmail.com
  */
 
-public class RepoListInteractorImpl implements IRepoListInteractor {
+public class RepoListInteractorImpl implements IRepoListInteractor, GithubRepositoriesCallback {
 
     private RepoListListener mListener;
 
-    @Inject
-    public RepoListInteractorImpl() {
+    ApiManager mApiManager;
 
+    @Inject
+    public RepoListInteractorImpl(ApiManager apiManager) {
+        this.mApiManager = apiManager;
     }
 
     @Override
     public void fetchGithubRepos(RepoListListener listListener) {
         this.mListener = listListener;
+        mApiManager.fetchGithubRepositories(this);
+    }
+
+    @Override
+    public void onSuccess(List<GithubRepoModel> repositories) {
+        mListener.onSuccess(repositories);
+    }
+
+    @Override
+    public void onError(String msg) {
+        mListener.onError(msg);
     }
 }
