05afa47e96d51e373ea65ce6c86ce12728dc01f7
Alen Siljan <alen.siljan@gmail.com>
2017-02-20 21:24:08 +0100

    Update BaseFragment class

--- app/src/main/java/com/siljan/alen/mvpdaggersample/common/BaseFragment.java
+++ app/src/main/java/com/siljan/alen/mvpdaggersample/common/BaseFragment.java
@@ -3,6 +3,13 @@ package com.siljan.alen.mvpdaggersample.common;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.View;
+
+import com.siljan.alen.mvpdaggersample.MyApplication;
+import com.siljan.alen.mvpdaggersample.di.components.GithubAppComponent;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by Alen Siljan on 20.2.2017..
@@ -11,10 +18,35 @@ import android.support.v4.app.Fragment;
 
 public abstract class BaseFragment extends Fragment {
 
+    protected View mView;
+    protected Unbinder mUbinder;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        injectDependencies(MyApplication.getInstance().getAppComponent());
     }
 
-    protected abstract void injectDependencies();
+    protected void bindViews() {
+        ButterKnife.bind(this, mView);
+    }
+
+    protected abstract void injectDependencies(GithubAppComponent appComponent);
+
+    protected void showLoadingLayout() {
+
+    }
+
+    protected void hideLoadingLayout() {
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (mUbinder != null) {
+            mUbinder.unbind();
+        }
+    }
 }
