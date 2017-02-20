e55920e05b760ddcc845a6662c692296ed7e3ee1
Alen Siljan <alen.siljan@gmail.com>
2017-02-20 21:56:25 +0100

    Update RepoListvViewHolder class

---
+++ app/src/main/java/com/siljan/alen/mvpdaggersample/repos/viewholders/RepoListViewHolder.java
@@ -0,0 +1,44 @@
+package com.siljan.alen.mvpdaggersample.repos.viewholders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.siljan.alen.mvpdaggersample.R;
+import com.siljan.alen.mvpdaggersample.data.models.GithubRepoModel;
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Alen Siljan on 20.2.2017..
+ * alen.siljan@gmail.com
+ */
+
+public class RepoListViewHolder extends RecyclerView.ViewHolder {
+
+    @BindView(R.id.listItemRepoName)
+    TextView mRepoName;
+    @BindView(R.id.listItemRepoAvatar)
+    ImageView mRepoAvatar;
+    @BindView(R.id.listItemRepoDescr)
+    TextView mRepoDescr;
+
+    public RepoListViewHolder(View itemView) {
+        super(itemView);
+
+        ButterKnife.bind(this, itemView);
+    }
+
+    public void bindData(GithubRepoModel repoModel, Picasso picasso) {
+        mRepoName.setText(String.format("%s", repoModel.getFullName()));
+        mRepoDescr.setText(String.format("%s", repoModel.getRepoDescr()));
+
+        picasso
+                .load(repoModel.getGithubUser().getAvatarUrl())
+                .into(mRepoAvatar);
+//                .placeholder(R.drawable.)
+    }
+}
