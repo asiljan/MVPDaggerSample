a6b2ba13af775e2a79636934b3dc98b64021bdd6
Alen Siljan <alen.siljan@gmail.com>
2017-02-20 20:41:55 +0100

    Update Application class

--- app/src/main/java/com/siljan/alen/mvpdaggersample/MyApplication.java
+++ app/src/main/java/com/siljan/alen/mvpdaggersample/MyApplication.java
@@ -2,6 +2,20 @@ package com.siljan.alen.mvpdaggersample;
 
 import android.app.Application;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.jakewharton.picasso.OkHttp3Downloader;
+import com.siljan.alen.mvpdaggersample.di.components.DaggerGithubAppComponent;
+import com.siljan.alen.mvpdaggersample.di.components.GithubAppComponent;
+import com.siljan.alen.mvpdaggersample.di.modules.AppModule;
+import com.siljan.alen.mvpdaggersample.networking.GithubApiService;
+import com.squareup.picasso.Picasso;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by Alen Siljan on 20.2.2017..
  * alen.siljan@gmail.com
@@ -9,8 +23,30 @@ import android.app.Application;
 
 public class MyApplication extends Application {
 
+    private static MyApplication instance;
+    private GithubAppComponent appComponent;
+
     @Override
     public void onCreate() {
         super.onCreate();
+
+        setInstance(this);
+        //// TODO: 20.2.2017. init other stuff i.e fabric
+
+        appComponent = DaggerGithubAppComponent.builder()
+                .appModule(new AppModule(this))
+                .build();
+    }
+
+    public static void setInstance(MyApplication instance) {
+        MyApplication.instance = instance;
+    }
+
+    public static MyApplication getInstance() {
+        return instance;
+    }
+
+    public GithubAppComponent getAppComponent() {
+        return appComponent;
     }
 }
