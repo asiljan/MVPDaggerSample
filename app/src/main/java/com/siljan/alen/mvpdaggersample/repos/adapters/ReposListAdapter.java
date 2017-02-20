b7213007725720b88079a598593e50367ec85e9c
Alen Siljan <alen.siljan@gmail.com>
2017-02-20 21:55:59 +0100

    Update repoList adapter class

--- app/src/main/java/com/siljan/alen/mvpdaggersample/repos/adapters/ReposListAdapter.java
+++ app/src/main/java/com/siljan/alen/mvpdaggersample/repos/adapters/ReposListAdapter.java
@@ -2,9 +2,13 @@ package com.siljan.alen.mvpdaggersample.repos.adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
+import com.siljan.alen.mvpdaggersample.R;
 import com.siljan.alen.mvpdaggersample.data.models.GithubRepoModel;
+import com.siljan.alen.mvpdaggersample.repos.viewholders.RepoListViewHolder;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -30,16 +34,30 @@ public class ReposListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item_repo_list, parent, false);
+
+        return new RepoListViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        GithubRepoModel model = mRepositories.get(position);
 
+        RepoListViewHolder viewHolder = (RepoListViewHolder) holder;
+        viewHolder.bindData(model, mPicasso);
     }
 
     @Override
     public int getItemCount() {
         return mRepositories != null ? mRepositories.size() : 0;
+    }
+
+    public void updateList(List<GithubRepoModel> repositories) {
+        mRepositories.clear();
+
+        mRepositories.addAll(repositories);
+
+        notifyDataSetChanged();
     }
 }
