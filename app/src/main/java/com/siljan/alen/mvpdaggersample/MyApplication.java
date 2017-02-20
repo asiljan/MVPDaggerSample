f4030330464f6299b26fc6108aa607e81f550a7f
Alen Siljan <alen.siljan@gmail.com>
2017-02-20 18:59:00 +0100

    Add Application class

---
+++ app/src/main/java/com/siljan/alen/mvpdaggersample/MyApplication.java
@@ -0,0 +1,16 @@
+package com.siljan.alen.mvpdaggersample;
+
+import android.app.Application;
+
+/**
+ * Created by Alen Siljan on 20.2.2017..
+ * alen.siljan@gmail.com
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- app/src/main/java/com/siljan/alen/mvpdaggersample/navigation/MainActivity.java
+++ app/src/main/java/com/siljan/alen/mvpdaggersample/navigation/MainActivity.java
@@ -1,7 +1,9 @@
-package com.siljan.alen.mvpdaggersample;
+package com.siljan.alen.mvpdaggersample.navigation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.siljan.alen.mvpdaggersample.R;
 
 public class MainActivity extends AppCompatActivity {
 
